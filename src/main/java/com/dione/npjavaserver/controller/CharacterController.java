e04df047eadca4c3038a090ee591b32f616f37bf
Michele de Meester <demeestermichele@gmail.com>
2022-09-28 22:16:19 +0200

    only list works

--- src/main/java/com/dione/npjavaserver/controller/CharacterController.java
+++ src/main/java/com/dione/npjavaserver/controller/CharacterController.java
@@ -6,79 +6,47 @@ import com.dione.npjavaserver.model.Sex;
 import com.dione.npjavaserver.repository.CharacRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 import java.util.Set;
 
-@RestController
+@Controller
 @RequestMapping("/characters")
 @CrossOrigin(origins = "*")
 public class CharacterController {
 
+    private final CharacRepository characRepository;
+
     @Autowired
-    private CharacRepository characRepository;
+    public CharacterController(CharacRepository characRepository) {
+        this.characRepository = characRepository;
+    }
+
 
     @GetMapping("/characterList")
-    public String showCharacterList(Model character) {
-        character.addAttribute("characters", characRepository.findAll());
-        return characRepository.findAll().toString();
+    public String showCharacterList(Model model) {
+        model.addAttribute("characters", characRepository.findAll());
+        return "characters/character-list";
     }
 
     /**
      * Add new character to table "character"
-     *
-     * @param first  String first name of the character.
-     * @param last   String last name of the character.
-     * @param role   Enumeration what role does the character play in the story options: PROTAGONIST, ANTAGONIST, SIDE, BACKGROUND, THROWAWAY
-     * @param sex    Enumeration what is the sex of the character, options: MALE, FEMALE, THERIAN (shape-shifter)
-     * @param mother Character the biological mother of the character, input the ID of the mother
-     * @param father Character the biological father of the character, input the ID of the father
      * @return verification message.
      * @throws Exception for try-catch
      */
     @PostMapping("/add")
-    public String addCharacter(@RequestParam String first, @RequestParam String last, @RequestParam Role role, @RequestParam Sex sex, @RequestParam Charac mother, @RequestParam Charac father) throws Exception {
-        Charac character = new Charac();
-        character.setFirstName(first);
-        character.setLastName(last);
-        try {
-            //do this in client
-            // String role1 = role.toString().toUpperCase();
-            character.setRole(role);
-        } catch (ConversionFailedException e) {
-            System.err.println("Role needs to be in capitals" + e);
-        }
-        //TODO add gender
-        try {
-            character.setSex(sex);
-        } catch (Exception e) {
-            System.err.println("error " + e);
-            throw new Exception();
+    public String addCharacter(@Valid Charac charac, BindingResult result, Model model){
+        if (result.hasErrors()) {
+            return "add-character";
         }
 
-        //TODO well this isn't very PC. add "legal guardians"
-
-        if (mother.getSex() != Sex.MALE) {
-            try {
-                character.setMother(mother);
-            } catch (Exception e) {
-                throw new Exception(mother.getFirstName() + " is male and cannot be the biological mother");
-            }
-        }
-
-        if (father.getSex() != Sex.FEMALE) {
-            try {
-                character.setFather(father);
-
-            } catch (Exception e) {
-                throw new Exception(father.getFirstName() + " is female and cannot be the biological father.");
-            }
-
-        }
-        characRepository.save(character);
-        return "Added new character to repo!";
+        characRepository.save(charac);
+        model.addAttribute("charac", characRepository.findAll());
+        return "redirect:/index";
     }
 
     /**
@@ -99,7 +67,7 @@ public class CharacterController {
         Charac character = findCharacterById(id);
         if (character == null) {
             character = new Charac();
-            this.addCharacter(first, last, role, sex, mother, father);
+//            this.addCharacter(first, last, role, sex, mother, father);
         } else {
             if (character.getFirstName() != null) {
                 character.setFirstName(first);
@@ -153,19 +121,28 @@ public class CharacterController {
      *
      * @return json list of characters
      */
+/*
     @GetMapping("/list")
     public List<Charac> getCharacters() {
         return (List<Charac>) characRepository.findAll();
     }
+*/
 
-
-    @GetMapping("/{id}")
-    public Charac findCharacterById(@PathVariable Integer id) {
+    public Charac findCharacterById(Integer id) {
         return characRepository.findCharacById(id);
     }
 
+    @GetMapping("/{id}")
+    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
+        Charac charac = characRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid character Id:" + id));
+        model.addAttribute("charac", charac);
+        return "characters/character";
+    }
+
+
     /**
      * find all characters with the same parent id
+     *
      * @param id of the character whose kids we want to find
      * @return list of characters from the id of parent
      */
@@ -175,7 +152,7 @@ public class CharacterController {
             return characRepository.findCharacsByMother(characRepository.findCharacById(id));
         } else if (findCharacterById(id).getSex() == Sex.MALE) {
             return characRepository.findCharacsByFather(characRepository.findCharacById(id));
-        }else if( findCharacterById(id).getSex() == Sex.THERIAN){
+        } else if (findCharacterById(id).getSex() == Sex.THERIAN) {
             Set<Charac> charSex = characRepository.findCharacsByFather(characRepository.findCharacById(id));
             charSex.addAll(characRepository.findCharacsByMother(characRepository.findCharacById(id)));
             return charSex;
