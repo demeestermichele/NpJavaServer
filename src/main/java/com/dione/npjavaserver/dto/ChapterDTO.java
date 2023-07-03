2aca2b8109854bdb00a9243303cd9cfb592c814c
Michele de Meester <demeestermichele@gmail.com>
2023-07-03 10:19:53 +0200

    chapters done

--- src/main/java/com/dione/npjavaserver/dto/ChapterDTO.java
+++ src/main/java/com/dione/npjavaserver/dto/ChapterDTO.java
@@ -33,6 +33,14 @@ public class ChapterDTO implements Serializable {
         this.book = book;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
